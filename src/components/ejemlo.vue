229a37570f0dffff85c0fde2320ce1404e971bb1
Vitoria <vitoria.oli.san14@gmail.com>
2022-12-13 13:35:04 +0100

    estructura del formulario

--- src/components/ejemlo.vue
+++ src/components/ejemlo.vue
@@ -2,10 +2,51 @@
 </script>
 
 <template>
-    <div>
-        
+    <div id="form">
+        <h1>Add Hero</h1>
+        <img src="../assets/Perfil .jpg" alt="" id="foto">
+        <input type="text" id="name">
+        <input type="text" id="univers">
+        <input type="text" id="info">
+        <button id=" add">Add</button>
     </div>
 </template>
 <style lang="scss" scoped>
+
+#form{
     
+    display: flex;
+    justify-content: center; 
+    flex-direction: column;
+    width: 30%;
+    height: 65%;
+    background-color: #E34038;
+    float: right ;
+    margin-right: 3%;
+
+
+}
+#name{ 
+   height: 10%; 
+   background-color: #155FCF;
+}
+#univers{
+    height: 10%;
+    background-color: #155FCF;
+}
+#info{
+    height: 10%;
+    background-color: #155FCF;
+}
+h1{
+    
+    text-align: center;
+}
+#add{
+
+    background-color: #FFE7E2;
+}
+#foto{
+    
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+import ejemlo from '../components/ejemlo.vue';
 </script>
 
 <template>
   <main>
-
+    <ejemlo />
   </main>
 </template>
+<style lang="scss">
+main{
+    width: 100%;
+    height: 100%;
+}
+</style>
