94c36147bc6c4b69a8ff08247ce6b83423e46f15
Vitoria <vitoria.oli.san14@gmail.com>
2022-12-15 12:58:51 +0100

    creation header

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
 
       <nav>
@@ -25,8 +24,7 @@ header {
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+ 
 }
 
 nav {
