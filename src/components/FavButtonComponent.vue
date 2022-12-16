427ca911902f821abc0c8678d80fd57068f7e498
Bryan <bryanpjpgi2b@hotmail.com>
2022-12-16 15:00:02 +0100

    Array fakeado

--- src/components/FavButtonComponent.vue
+++ src/components/FavButtonComponent.vue
@@ -5,13 +5,22 @@ function moveElements(){
   const clone = node.cloneNode(true);
   const copied = document.getElementById("aboutView");
   copied.appendChild(clone);
+  const nodecopied = copied.appendChild(clone);
 }
-  
+
+
 </script>
 
 <template>
   <div id="component-button"></div>
     <div>
-      <button v-on:click="moveElements">Mueve un parrafo con texto.</button>
+      <button v-on:click="moveElements">Añadir a Favoritos</button>
 </div>
 </template>
+
+<style>
+a {
+  background: none;
+  border: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,18 @@
 import FavButtonComponent from '../components/FavButtonComponent.vue';
 import AboutView from './AboutView.vue';
 
+let heroes = [
+{
+  "id": 1,
+  "name": "A-Bomb",
+  "slug": "1-a-bomb",
+  },
+  {
+  "id": 2,
+  "name": "Batman",
+  "slug": "1-a-bomb",
+  },
+]
 </script>
 
 <template>
@@ -9,6 +21,9 @@ import AboutView from './AboutView.vue';
     <div id="homeView"><p>Hola Mundo</p></div>
     <div id="copied"></div>
     <FavButtonComponent/>
+    <div v-for="item in items">
+
+    </div>
     <AboutView id="aboutView"/>
   </main>
 </template>
